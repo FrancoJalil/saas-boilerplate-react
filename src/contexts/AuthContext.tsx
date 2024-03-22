c2e7437c9c61ae0011630fb69092c5988c0e92cb
Franco Jalil <jalilfranco9@gmail.com>
2024-03-22 00:42:40 -0300

    :bulb: delete random comment

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -90,7 +90,6 @@ export const AuthProvider = ({ children }: Props) => {
 
 
       if (response.status === 200) {
-        console.log("REFRESCADO REY")
         setAuthTokens(data)
         setUser(jwtDecode(data.access))
         localStorage.setItem('authTokens', JSON.stringify(data))
